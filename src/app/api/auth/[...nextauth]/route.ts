330ccf2ef395630c0012c38214913671ac6752bb
mahesh-143 <maheshodedara13@yahoo.com>
2023-07-04 16:19:42 +0530

    x

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -64,4 +64,4 @@ const authOptions: NextAuthOptions = {
 }
 
 const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST, authOptions }
+export { handler as GET, handler as POST }
