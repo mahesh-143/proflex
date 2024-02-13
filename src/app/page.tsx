43d4aa864737d9201393c760f50a949d176d070a
mahesh-143 <maheshodedara13@yahoo.com>
2024-02-13 16:01:24 +0530

    Displaying projects on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
 import Hero from "@/components/sections/hero";
+import { Category } from "@prisma/client";
+import { fetchProjects } from "./projects/actions";
+import { Filter } from "@/components/categories";
+import { InfiniteScrollProjects } from "./projects/InfiniteScrollProjects";
+import { PagesRouteModuleOptions } from "next/dist/server/future/route-modules/pages/module.compiled";
 
-export default async function Home() {
+type SearchParams = {
+  category?: Category;
+  [key: string]: string | string[] | undefined;
+};
+
+type Props = {
+  searchParams: SearchParams;
+};
+
+export default async function Home({ searchParams }: Props) {
+  const search =
+    typeof searchParams.search === "string" ? searchParams.search : undefined;
+
+  const projects = await fetchProjects({
+    search,
+    category: searchParams.category,
+  });
   return (
     <div className="mx-4">
       <Hero />
+      <Filter>
+        <InfiniteScrollProjects search={search} initialProjects={projects} />
+      </Filter>
     </div>
   );
 }
