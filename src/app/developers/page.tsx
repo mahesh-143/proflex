8a065ad843c9f8d30720816c48dc011012d6443b
mahesh-143 <maheshodedara13@yahoo.com>
2024-02-17 21:20:58 +0530

    Fixed prsima schema

--- src/app/developers/page.tsx
+++ src/app/developers/page.tsx
@@ -1,37 +1,52 @@
-import React from "react"
-import prisma from "../libs/prismadb"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import Link from "next/link"
+import React from "react";
+import prisma from "../libs/prismadb";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
+import { UserType } from "@prisma/client";
 
 async function developers() {
-  const users = await prisma.user.findMany()
+  const developer = await prisma.user.findMany({
+    where: {
+      UserType: "Developer",
+    },
+    include: {
+      Developer: {
+        select: {
+          role: true,
+        },
+      },
+    },
+  });
+
   return (
     <div className="p-4 mx-auto md:max-w-4xl lg:max-w-5xl">
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        {users &&
-          users.map((user) => {
+        {developer &&
+          developer.map((developer) => {
             return (
               <Link
-                href={`user/${user.id}`}
-                key={user.id}
+                href={`user/${developer.id}`}
+                key={developer.id}
                 className="flex items-center space-x-4 p-4 border border-slate-200 rounded-xl overflow-clip"
               >
                 <Avatar>
-                  <AvatarImage src={user.image || "/default.webp"} />
-                  <AvatarFallback>{user.name?.charAt(0)}</AvatarFallback>
+                  <AvatarImage src={developer.image || "/default.webp"} />
+                  <AvatarFallback>{developer.name?.charAt(0)}</AvatarFallback>
                 </Avatar>
                 <div>
                   <p className="text-sm font-medium leading-none">
-                    {user.name}
+                    {developer.name}
                   </p>
-                  <p className="text-sm text-muted-foreground">{user.role} Developer</p>
+                  <p className="text-sm text-muted-foreground">
+                    {developer.Developer?.role} Developer
+                  </p>
                 </div>
               </Link>
-            )
+            );
           })}
       </div>
     </div>
-  )
+  );
 }
 
-export default developers
+export default developers;
