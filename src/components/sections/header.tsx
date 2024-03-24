553f6bdda0e40472b46973e8139acfc8676bfa70
mahesh-143 <maheshodedara13@yahoo.com>
2024-03-24 18:28:19 +0530

    Revert "x"
    This reverts commit 6090c7aa6039d52fc9d43996effebb1e49f23090.

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -1,34 +1,25 @@
-// "use client";
-import { MainNav } from "@/components/main-nav";
-import { UserNav } from "@/components/user-nav";
-import Logo from "@/components/logo";
-import { Button } from "../ui/button";
-import { CreateAccount } from "../authentication/register-user";
-import { Login } from "../authentication/login";
-import { useSession } from "next-auth/react";
-import Link from "next/link";
-import MobileNav from "../mobile-nav";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/lib/session";
+"use client"
+import { MainNav } from "@/components/main-nav"
+import { UserNav } from "@/components/user-nav"
+import Logo from "@/components/logo"
+import { Button } from "../ui/button"
+import { CreateAccount } from "../authentication/register-user"
+import { Login } from "../authentication/login"
+import { useSession } from "next-auth/react"
+import Link from "next/link"
+import MobileNav from "../mobile-nav"
 
-const Header = async () => {
-  // const session = useSession();
-  const session = await getServerSession(authOptions);
-  //
-  // const name: string = session.data?.user?.name || "";
-  // const email: string = session.data?.user?.email || "";
-  // const image: string = session.data?.user?.image || "";
-  //
-  //
-  const name: string = session?.user.name || "";
-  const email: string = session?.user.email || "";
-  const image: string = session?.user.image || "";
+const Header = () => {
+  const session = useSession()
+  const name: string = session.data?.user?.name || ""
+  const email: string = session.data?.user?.email || ""
+  const image: string = session.data?.user?.image || ""
 
   const userProps = {
     name,
     email,
     image,
-  };
+  }
   return (
     <div className="flex-col md:flex">
       <div className="border-b">
@@ -59,7 +50,7 @@ const Header = async () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
